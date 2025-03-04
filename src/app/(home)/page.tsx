f14cece16fe064e5bf78f328d17fc280df7570d1
Pedro Jorge <pedrojorge2011@gmail.com>
2025-03-04 15:07:53 -0300

    implementation basic layout

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,5 +1,6 @@
 import { Button } from '@/components/ui/button'
+import Image from 'next/image'
 
 export default function Home() {
-  return <Button variant="secondary">Hello World</Button>
+  return <div>Load videos</div>
 }
---
+++ src/modules/home/ui/components/home-sidebar/personal-section.tsx
@@ -0,0 +1,59 @@
+'use client'
+
+import {
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from '@/components/ui/sidebar'
+import { FlameIcon, HistoryIcon, HomeIcon, ListVideoIcon, PlaySquareIcon, ThumbsUpIcon } from 'lucide-react'
+import Link from "next/link"
+
+const items = [
+  {
+    title: 'History',
+    url: '/playlists/history',
+    icon: HistoryIcon,
+  },
+  {
+    title: 'Liked videos',
+    url: '/playlists/likes',
+    icon: ThumbsUpIcon,
+    auth: true,
+  },
+  {
+    title: 'All playlists',
+    url: '/playlists',
+    icon: ListVideoIcon,
+    auth: true,
+  },
+]
+
+export function PersonalSection() {
+  return (
+    <SidebarGroup>
+      <SidebarGroupLabel>You</SidebarGroupLabel>
+      <SidebarGroupContent>
+        <SidebarMenu>
+          {items.map(item => (
+            <SidebarMenuItem key={item.title}>
+              <SidebarMenuButton
+                tooltip={item.title}
+                asChild
+                isActive={false} // TODO: Change to look at current pathname
+                onClick={() => {}} // TODO: add funtion
+              >
+                <Link href={item.url} className="flex items-center gap-4">
+                  <item.icon />
+                  <span className="text-sm">{item.title}</span>
+                </Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          ))}
+        </SidebarMenu>
+      </SidebarGroupContent>
+    </SidebarGroup>
+  )
+}
